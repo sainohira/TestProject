4bd0f45bd92ee062f751af65c08cd442410fed72
斉野平翔太 <xsainohira.shota@contract.isid.co.jp>
2016-04-06 17:44:10 +0900

    test

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //シャッフルする配列
+            var ary = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27 };
+
+            //シャッフルする
+            int[] ary2 = ary.OrderBy(i => Guid.NewGuid()).ToArray();
+
+            Console.WriteLine(ary2[0].ToString());
+            Thread.Sleep(5000);
+        }
+    }
+}
